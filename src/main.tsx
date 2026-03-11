86d50ec04c6875fe4400bdfef8fbf99702d3a6ff
Tanvir <tanvirmern@gmail.com>
2026-03-11 19:42:44 +0600

    add

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { HelmetProvider } from 'react-helmet-async';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 // We fetch the client ID from environment variables, fallback is handled if missing
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "";
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "186963743448-d73odfsqgf0s8gsv9ncmd7nivha9ko3u.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
