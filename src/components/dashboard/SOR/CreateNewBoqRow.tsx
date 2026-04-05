fc54c951b33838bc8190552d72fbca7412335a2e
Tanvir <tanvirmern@gmail.com>
2026-04-06 01:25:25 +0600

    ad

--- src/components/dashboard/SOR/CreateNewBoqRow.tsx
+++ src/components/dashboard/SOR/CreateNewBoqRow.tsx
@@ -14,6 +14,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import { Plus } from "lucide-react"; // Optional: for a better trigger icon
+import { BOQItem } from "@/pages/dashboardLayout/pg22aOtmGoods/TenderTabs/BOQTab";
 
 export function CreateNewBoqRow({
   tableId,
